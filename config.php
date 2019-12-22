25a964e5ec608e4da858b758d536027e4e5008e5
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-22 18:52:17 +0700

    update search

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $port = '5432';
 $db = 'dfg9jcitt5aedg';
 $username = 'zcqccnpdcwmhru';
 $password = 'd75b3b8c75baf02745f9a2633801e350bc42e76c3c23bf036a5a7a9bb18819cb';
-$conn = pg_connect("host='$server', port='$port', dbname='$db', user='$username', password='$password'")
+$conn = pg_connect("host='$server' dbname='$db' user='$username' password='$password'")
 // mysqli_set_charset($conn, 'UTF8');
 // mysqli_select_db($conn, $db);
 
---
+++ search.php
@@ -0,0 +1,65 @@
+
+<?php 	 	 	
+$and="";
+if (isset($_GET['idl'])) {
+	$and="and id_kindbook='$_GET[idl]'";
+}
+
+$searchText="";
+if (isset($_POST['searchText'])) {
+	$searchText=$_POST['searchText'];
+}
+
+$sql_search="select * from book where bookname LIKE '%$searchText%' " . $and;
+$query_search=pg_query($conn, $sql_search);
+
+$line_kind="";
+if (isset($_GET['idl'])) {
+	$sql_kind="select * from kindbook where id='$_GET[idl]'";
+	$query_kind=pg_query($conn, $sql_kind);
+	$line_kind=pg_fetch_assoc($query_kind);
+}
+?>
+
+<hr>
+
+<?php
+if ($line_kind) { ?>
+	<p style="background: #e6e6e6; padding: 10px; border-left: 5px solid blue; font-weight: bold; font-size: 18px;"><?php echo $line_kind['kindbook'] ?>
+	</p>
+<?php
+}
+?>
+<div class="row">
+	<?php 
+	if($count=pg_num_rows($query_search)==0) { ?>
+		<h4>No Books Found</h4>
+		<?php 
+	}else{
+		?>
+		<?php
+		while ($line_search=pg_fetch_assoc($query_search)) {
+			?>
+			<div style="padding: 10px">
+				<div class="card align-items-center" style="width: 300px; text-align: center;">
+					<a href="index.php?xem=BookInfo&id=<?php echo $line_search['id'] ?>">
+						<div class="card-header">
+							<img src="images/<?php echo $line_search['img'] ?>" style="width: 250px">
+						</div>
+						<div class="card-body">
+							<p><?php echo $line_search['bookname'] ?></p>
+						</div>
+						<div class="card-footer">
+							<span style="color: red; font-size: 14px;">
+								<?php echo $line_search['price'].'VND' ?>									
+							</span>
+						</div>
+					</a>
+
+				</div>
+			</div>
+			<?php 
+		}
+	}
+	?>
+</div>
