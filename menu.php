bd5daff75af4615ea062601b1fb5b62871ef8c56
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-23 17:34:47 +0700

    2

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
 		<form class="form-inline" action="index.php" method="post" enctype="multipart/form-data">
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
 			<button type="submit" class="btn btn-success" name="searchButton">Search Toy</button>
-			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>>&nbsp;>&nbsp;>&nbsp;>&nbsp;>&nbsp;
+			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>>&nbsp;>&nbsp;>&nbsp;>&nbsp;>&nbsp;>&nbsp;>&nbsp;>&nbsp;
 			<input type="button" id="btnLogin" value="Login with Google"> 
 	
 		</form>
