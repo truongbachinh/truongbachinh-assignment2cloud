e6a1488635e795e0256b7aa608ec793a2ea8cd78
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-24 00:01:03 +0700

    aa

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <div class="progress" style=" height: 55px">
 	<div class="progress-bar bg-success row" style="width: 120%;">
-	<a href="index.php"><img src="name.PNG" alt="ATN Company" style="width:220px;height:42px;border:0"></a>
+	<a href="index.php"><img src="name.PNG" alt="ATN Company" ></a>
 		<form class="form-inline" action="index.php" method="post" enctype="multipart/form-data">
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
 			<button type="submit" class="btn btn-success" name="searchButton">Search Toy</button>
