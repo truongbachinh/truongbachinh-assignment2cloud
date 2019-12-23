a48a3bf660df95d9d81b1ae81929e1d9d9e30d2f
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-23 15:46:32 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
 			<button type="submit" class="btn btn-success" name="searchButton">Search Toy</button>
 			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>
-			<input type="button" id="btnLogin" value="Login with Google"> 
+			
     
 		</form>
 	</div>
