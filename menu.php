da1fa22551bfc56c3a98da2686406b8715ded444
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-23 12:53:34 +0700

    a

--- menu.php
+++ menu.php
@@ -1,22 +1,13 @@
 <div class="progress" style=" height: 55px">
 	<div class="progress-bar bg-success row" style="width: 120%;">
-		<h2>&nbsp;&nbsp;List Kind Of Book&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+		<h2>&nbsp;&nbsp;List Kind Of Toys&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+		<button type="submit" class="btn btn-success" name="searchButton">Search Toy</button>
 		<form class="form-inline" action="index.php" method="post" enctype="multipart/form-data">
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
-			<button type="submit" class="btn btn-success" name="searchButton">Search Book</button>
+	
 			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>
 			<input type="button" id="btnLogin" value="Login with Google"> 
-    <script>
-        $('#btnLogin').on('click',loginClicked);
-        function   loginClicked(){
-            var provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider).then(result=>{
-                if(result !=null){
-                    window.location.replace('admin.php');
-                }
-            })
-        }
-    </script>
+    
 		</form>
 	</div>
 </div>
