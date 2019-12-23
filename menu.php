b989e014c5356e9a194ddfeb9284aa65334c3b56
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-23 12:29:01 +0700

    aaaa

--- menu.php
+++ menu.php
@@ -5,6 +5,18 @@
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
 			<button type="submit" class="btn btn-success" name="searchButton">Search Book</button>
 			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>
+			<input type="button" id="btnLogin" value="Login with Google"> 
+    <script>
+        $('#btnLogin').on('click',loginClicked);
+        function   loginClicked(){
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(result=>{
+                if(result !=null){
+                    window.location.replace('admin.php');
+                }
+            })
+        }
+    </script>
 		</form>
 	</div>
 </div>
