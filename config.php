9a400618555cf147969ffb09b1d2d68e40464289
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-24 11:31:08 +0700

    aaaaa

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$server = 'ec2-54-83-1-101.compute-1.amazonaws.com';
+$server = 'ec2-54-221-243-211.compute-1.amazonaws.com';
 $port = '5432';
-$db = 'd6cac6q5pb9fml';
-$username = 'hrqetlilclbrou';
-$password = '049705d314bfa1551aa9456d5eca053977b81ca8dca003428d85481c260aa17a';
+$db = 'd8vs5hnqhbm7tn';
+$username = 'sbnnzwollxlljf';
+$password = '30c3fe77a1f17d26f4035bab8d3c719d8b39f56cef28f9fbe9555aa0b0082516';
 $conn = pg_connect("host='$server' dbname='$db' user='$username' password='$password'")
 // mysqli_set_charset($conn, 'UTF8');
 // mysqli_select_db($conn, $db);
