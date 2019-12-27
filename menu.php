fc22f1bfbd852b99031f4194984897fef58cd4cf
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2019-12-27 10:50:41 +0700

    zz

--- menu.php
+++ menu.php
@@ -5,9 +5,7 @@
 			<input type="text" class="form-control" name="searchText" value="<?php if(isset($_POST['searchText'])) echo $_POST['searchText'] ?>" style="width: 750px">
 			<button type="submit" class="btn btn-success" name="searchButton">Search Toy</button>
 			<button type="submit" class="btn btn-success" name="cart">Your Cart</button>
-			<a href="index.php"><button type="submit" class="btn btn-success" name="loginWithGoogle">Login with Google
-
-			</button>&nbsp;
+			<button type="submit" class="btn btn-success" name="loginWithGoogle">Login with Google</button>&nbsp;
 		</form>
 	</div>
 </div>
